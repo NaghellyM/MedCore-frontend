5eccca2825d5764328771a34535873a54b157135
Daniell-Estrada <nelson.1702013546@ucaldas.edu.co>
2025-10-05 20:27:16 -0500

    fix: unnecessary header parameters removed.

--- src/presentation/pages/admin/components/adminHeader.tsx
+++ src/presentation/pages/admin/components/adminHeader.tsx
@@ -1,12 +1,7 @@
 import UserHeader from "../../../components/globals/header";
 
 const AdminHeader = () => {
-    return (
-        <UserHeader
-        name="Fernando Gomez" 
-        role="Administrador del sistema"
-        />
-    );
+  return <UserHeader />;
 };
 
 export default AdminHeader;
--- src/presentation/pages/nurse/components/nurseheader.tsx
+++ src/presentation/pages/nurse/components/nurseheader.tsx
@@ -1,13 +1,7 @@
-import UserHeader from "../../../components/globals/header"
-
+import UserHeader from "../../../components/globals/header";
 
 const NurseHeader = () => {
-    return (
-        <UserHeader
-            name="Maria Lopez"
-            role="Enfermera"
-            avatarUrl="https://i.pravatar.cc/48?img=5"
-        />
-    )
-}
-export default NurseHeader
+  return <UserHeader />;
+};
+export default NurseHeader;
+
